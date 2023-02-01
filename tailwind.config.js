31d7bf463ee778d901dc2d0f47f27d7414dae3d3
jricot01 <jricot01@gmail.com>
2023-02-01 14:23:15 -0500

    Responsive restaurant landing page

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+      mobile: "555px",
+      tablet: "1195px",
+    },
+  },
+  plugins: [],
+};
